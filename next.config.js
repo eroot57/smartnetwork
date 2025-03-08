766180d63583518932ab4535faaba7ee3fafa4df
eroot57 <elianroot23@gmail.com>
2025-03-09 00:07:29 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+import('next').NextConfig
+const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_CROSSMINT_API_KEY: process.env.NEXT_PUBLIC_CROSSMINT_API_KEY,
   },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push('pino-pretty', 'encoding');
+    return config;
+  },
 };
+
+module.exports = nextConfig;
