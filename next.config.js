90d6fdbd2f8dc481119f1d03be57218153e7f4bb
eroot57 <elianroot23@gmail.com>
2025-03-09 00:23:07 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_CROSSMINT_API_KEY: process.env.NEXT_PUBLIC_CROSSMINT_API_KEY,
   },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push('pino-pretty', 'encoding');
-    return config;
-  },
 };
-
-module.exports = nextConfig;
