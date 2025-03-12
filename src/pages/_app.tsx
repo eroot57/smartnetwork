5b9536181db4d1d21af481c22e10e46226c1cbd5
eroot57 <elianroot23@gmail.com>
2025-03-12 22:51:22 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import { WalletProvider } from '@/context/WalletProvider';
+import WalletProvider from '@/context/WalletProvider';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
