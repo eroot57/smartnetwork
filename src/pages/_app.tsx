ba859dd95462ab3d1d3a7bd5505104487ea15bc1
eroot57 <elianroot23@gmail.com>
2025-03-12 23:04:46 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,27 @@
 import { AppProps } from 'next/app';
 import WalletProvider from '@/context/WalletProvider';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Custom logic to handle route changes if needed
+    const handleRouteChange = (url: string) => {
+      console.log(`App is changing to: ${url}`);
+    };
+
+    router.events.on('routeChangeStart', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <Router>
-      <WalletProvider>
-        <Component {...pageProps} />
-      </WalletProvider>
-    </Router>
+    <WalletProvider>
+      <Component {...pageProps} />
+    </WalletProvider>
   );
 };
 
