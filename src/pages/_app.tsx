eabea64c3033ede42ff2f23f08aee2e0f74804ef
eroot57 <elianroot23@gmail.com>
2025-03-12 22:47:22 +0100

    Create _app.tsx

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { AppProps } from 'next/app';
+import { WalletProvider } from '@/context/WalletProvider';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Router>
+      <WalletProvider>
+        <Component {...pageProps} />
+      </WalletProvider>
+    </Router>
+  );
+};
+
+export default MyApp;
